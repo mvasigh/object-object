d11f6813ef0e34fdf6cc1956c15e4f2c3ed5c49f
Mehdi Vasigh <mvasigh@liquidframeworks.com>
2018-12-16 09:32:49 -0600

    fix build error

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -20,16 +20,7 @@ class BlogPostTemplate extends React.Component {
                     title={`${post.frontmatter.title} | ${siteTitle}`}
                 />
                 <h1>{post.frontmatter.title}</h1>
-                <p
-                    style={{
-                        ...scale(-1 / 5),
-                        display: 'block',
-                        marginBottom: rhythm(1),
-                        marginTop: rhythm(-1)
-                    }}
-                >
-                    {post.frontmatter.date}
-                </p>
+                <p>{post.frontmatter.date}</p>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
                 <hr
                     style={{
