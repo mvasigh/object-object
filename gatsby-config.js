cac7a38fbe7c9a2fa393b405381f4f86dbf0f34e
Mehdi Vasigh <mvasigh@liquidframeworks.com>
2018-12-15 21:52:46 -0600

    remove typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,68 +1,62 @@
 module.exports = {
-  siteMetadata: {
-    title: 'mvsing.js',
-    author: 'Mehdi Vasigh',
-    description: 'A blog about JavaScript, Web Development, and life.',
-    siteUrl: 'https://mvasigh.netlify.com/'
-  },
-  pathPrefix: '/gatsby-starter-blog',
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
+    siteMetadata: {
+        title: '[object Object]',
+        author: 'Mehdi Vasigh',
+        description: 'A blog about JavaScript, Web Development, and life.',
+        siteUrl: 'https://mvasigh.netlify.com/'
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    pathPrefix: '/gatsby-starter-blog',
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 590
+                path: `${__dirname}/src/pages`,
+                name: 'pages'
             }
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590
+                        }
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`
+                        }
+                    },
+                    'gatsby-remark-prismjs',
+                    'gatsby-remark-copy-linked-files',
+                    'gatsby-remark-smartypants'
+                ]
             }
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants'
-        ]
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      }
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`
-      }
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography'
-      }
-    }
-  ]
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            }
+        },
+        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`
+            }
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`
+    ]
 };
