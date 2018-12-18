ad28a4f4ee48eb5a0d93ebfcaf960fd39496ca4a
Mehdi Vasigh <mvasigh@liquidframeworks.com>
2018-12-17 22:02:36 -0600

    update theme

--- src/components/styles/Theme.js
+++ src/components/styles/Theme.js
@@ -1,10 +1,19 @@
 const theme = {
-    white: '#E8E9EB',
     black: '#212121',
-    primary: '#1D2D44',
-    primaryDark: '#0D1321',
-    secondary: '#EF6461',
-    accent: '#EF476F'
+    primaryAccent: '#DF9986',
+    dark: '#392D3B',
+    darkAccent: '#8B8595',
+    shades: {
+        '0': '#fffcfc',
+        '10': '#fb9cab',
+        '20': '#f2507b',
+        '30': '#e1217d',
+        '40': '#c7099f',
+        '50': '#9200a6'
+    }
 };
+
+theme.primary = theme.shades['20'];
+theme.white = theme.shades['0'];
 
 export default theme;
