4e87265ddde16a46574a08995fb02c3e3a6b214d
Mehdi Vasigh <mvasigh@liquidframeworks.com>
2018-12-31 08:59:00 -0500

    implement filtering by tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
+import styled from 'styled-components';
 
 import Bio from '../components/Bio';
 import Layout from '../components/Layout';
 import Divider from '../components/styles/Divider';
 import ContentStyles from '../components/styles/ContentStyles';
-import Tag from '../components/Tag';
 import TagList from '../components/TagList';
+
+const FilterBarStyles = styled.div`
+    display: flex;
+    margin: 0.8rem 0;
+    .clearAll {
+        margin-left: 0.8rem;
+        font-family: 'IBM Plex Mono', monospace;
+        font-size: 0.8rem;
+        font-weight: 500;
+        padding: 0.2rem 0.4rem;
+        color: ${({ theme }) => theme.darkAccent};
+        cursor: pointer;
+    }
+`;
 
 class BlogIndex extends React.Component {
     constructor(props) {
@@ -17,37 +31,93 @@ class BlogIndex extends React.Component {
             filters: []
         };
 
-        this.handleTagClick = this.handleTagClick.bind(this);
+        this.handleAddFilter = this.handleAddFilter.bind(this);
+        this.handleRemoveFilter = this.handleRemoveFilter.bind(this);
+        this.handleClearAllFilters = this.handleClearAllFilters.bind(this);
     }
 
     componentDidMount() {
         // when tag is clicked on an individual post
-        const { filters } = this.state;
         const { location } = this.props;
-        const tag = location.state ? location.state.tag : null;
-
-        if (tag && !filters.includes(tag)) {
-            this.setState({
-                filters: [...filters, tag]
-            });
+        if (location.state && location.state.tag) {
+            this.handleAddFilter(location.state.tag);
         }
     }
 
-    handleTagClick(tag) {
+    handleAddFilter(tag) {
         const { filters } = this.state;
-        if (tag && !filters.includes(tag)) {
-            this.setState({
-                filters: [...filters, tag]
-            });
-        }
+        if (filters.includes(tag)) return;
+        this.setState({
+            filters: [...filters, tag]
+        });
+    }
+
+    handleRemoveFilter(tag) {
+        const { filters } = this.state;
+        if (!filters.includes(tag)) return;
+        filters.splice(filters.indexOf(tag), 1);
+        this.setState({ filters });
+    }
+
+    handleClearAllFilters() {
+        this.setState({ filters: [] });
+    }
+
+    renderFilterBar() {
+        const { filters } = this.state;
+        const hasFilters = filters.length > 0;
+        return (
+            hasFilters && (
+                <FilterBarStyles>
+                    <TagList
+                        tags={filters}
+                        onTagClick={this.handleRemoveFilter}
+                    />
+                    <span
+                        className="clearAll"
+                        role="link"
+                        onClick={this.handleClearAllFilters}
+                    >
+                        clear all
+                    </span>
+                </FilterBarStyles>
+            )
+        );
+    }
+
+    renderPosts() {
+        const { filters } = this.state;
+        const posts = this.props.data.allMarkdownRemark.edges.filter(
+            ({ node }) =>
+                filters.every(tag => node.frontmatter.tags.includes(tag))
+        );
+
+        return posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug;
+            return (
+                <div key={node.fields.slug}>
+                    <h2 className="title">
+                        <Link to={node.fields.slug}>{title}</Link>
+                    </h2>
+                    <p className="date">{node.frontmatter.date}</p>
+                    <TagList
+                        tags={node.frontmatter.tags}
+                        onTagClick={this.handleAddFilter}
+                    />
+                    <p
+                        dangerouslySetInnerHTML={{
+                            __html: node.excerpt
+                        }}
+                    />
+                </div>
+            );
+        });
     }
 
     render() {
         const { data } = this.props;
-        const { filters } = this.state;
         const siteTitle = data.site.siteMetadata.title;
         const siteDescription = data.site.siteMetadata.description;
-        const posts = data.allMarkdownRemark.edges;
 
         return (
             <Layout location={this.props.location} title={siteTitle}>
@@ -56,29 +126,8 @@ class BlogIndex extends React.Component {
                     meta={[{ name: 'description', content: siteDescription }]}
                     title={siteTitle}
                 />
-                <ContentStyles>
-                    {posts.map(({ node }) => {
-                        const title =
-                            node.frontmatter.title || node.fields.slug;
-                        return (
-                            <div key={node.fields.slug}>
-                                <h2 className="title">
-                                    <Link to={node.fields.slug}>{title}</Link>
-                                </h2>
-                                <p className="date">{node.frontmatter.date}</p>
-                                <TagList
-                                    tags={node.frontmatter.tags}
-                                    onTagClick={this.handleTagClick}
-                                />
-                                <p
-                                    dangerouslySetInnerHTML={{
-                                        __html: node.excerpt
-                                    }}
-                                />
-                            </div>
-                        );
-                    })}
-                </ContentStyles>
+                {this.renderFilterBar()}
+                <ContentStyles>{this.renderPosts()}</ContentStyles>
                 <Divider />
                 <Bio />
             </Layout>
